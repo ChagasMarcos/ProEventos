e067f438df2009bd0c2727a1da9782a7609c5637
ChagasMarcos <antunesm@hotmai.com>
2025-09-28 14:37:07 -0300

    alteracoesAngular

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,31 @@ import { Component } from '@angular/core';
 })
 export class EventosComponent {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg: number = 80;
+  marginImg: number = 2;
+
+  imgToggleBtn : boolean = true;
+  toggleBtnIcon = "fa-solid fa-eye-slash";
+  private _filtroLista: string = '';
+
+public get filtroLista(): string {
+return this._filtroLista;
+};
+
+public set filtroLista(value:string) {
+this._filtroLista = value;
+this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+}
+
+filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLowerCase().indexOf(filtrarPor) !== -1
+    )
+}
 
   constructor(private http: HttpClient) {}
 
@@ -16,14 +40,23 @@ export class EventosComponent {
     this.getEventos();
   }
 
+  public toglleBtnImg() {
+    this.imgToggleBtn = !this.imgToggleBtn;
+    if (this.imgToggleBtn == true ) {
+      this.toggleBtnIcon = "fa-solid fa-eye-slash"
+    } else {
+      this.toggleBtnIcon = "fa-solid fa-eye"
+    }
+  }
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error),
     );
-
-    console.log(this.eventos);
   }
 
 }
---
+++ Front/ProEventos-App/src/app/models/evento.ts
@@ -0,0 +1,9 @@
+export class Evento {
+  // number : EventoId,
+  // string : Local,
+  // string : DataEvento,
+  // string : Tema,
+  // int : QtdPessoas,
+  // string : Lote,
+  // string : ImagemURL,
+}
